e21b8e2d5cf2af5f68dd8fbc1787feef6b0a2916
Sebastian Rahlf <basti@redtoad.de>
2017-04-14 11:25:23 +0200

    Add PUT endpoint.

--- test/server.js
+++ test/server.js
@@ -22,27 +22,99 @@ describe('API', function () {
 
     describe('GET list of wines', function () {
 
-    it('returns all wines', sinon.test(function (done) {
-      this.stub(Wine, 'find', function mockFind (opt, cb) { cb(null, wineList); });
-      api().get('/wines')
-        .expectStatus(200)
-        .end(function (err, res, body) {
-          if (err) throw err;
-            done();
-        });
-    }));
+      it('returns all wines', sinon.test(function (done) {
+        this.stub(Wine, 'find', function mockFind (opt, cb) { cb(null, wineList); });
+        api().get('/wines')
+          .expectStatus(200)
+          .end(function (err, res, body) {
+            if (err) throw err;
+              done();
+          });
+      }));
 
-    it('will ignore invalid query parameters', sinon.test(function (done) {
-      this.stub(Wine, 'find', function mockFind (opt, cb) { cb(null, wineList); });
-      api().get('/wines?something=')
-        .expectStatus(200)
-        .end(function(err, res, body) {
-          if (err) throw err;
-          assert.equal(body.length, 2);
-          done();
-        });
-    }));
+      it('will ignore invalid query parameters', sinon.test(function (done) {
+        this.stub(Wine, 'find', function mockFind (opt, cb) { cb(null, wineList); });
+        api().get('/wines?something=')
+          .expectStatus(200)
+          .end(function(err, res, body) {
+            if (err) throw err;
+            assert.equal(body.length, 2);
+            done();
+          });
+      }));
 
     });
+
+    describe('POST add new wines', function () {
+
+      describe('while validating', sinon.test(function () {
+
+        this.stub(Wine.prototype, 'save', function mockSave (cb) {
+          console.log(this);
+          return this.validate(cb);
+        });
+
+        it('will fail for missing required fields', sinon.test(function (done) {
+          api().post('/wines')
+            .send({missing: 'everything'})
+            .expectStatus(400)
+            .end(function (err, res, body) {
+              assert.equal(body.error, 'VALIDATION_ERROR');
+              assert.equal(body.validation.year, 'MISSING');
+              assert.equal(body.validation.type, 'MISSING');
+              assert.equal(body.validation.name, 'MISSING');
+              assert.equal(body.validation.country, 'MISSING');
+              done();
+            });
+        }));
+
+        // it('will not fail for missing optional fields', sinon.test(function (done) {
+        //   api().post('/wines')
+        //     .send({year: '2015', type: 'red', name: 'Wine', country: 'France'})
+        //     .expectStatus(200)
+        //     .end(function (err, res, body) {
+        //       assert.equal(body.error, undefined);
+        //       done();
+        //     });
+        // }));
+
+        it('will fail for invalid type value', sinon.test(function (done) {
+          api().post('/wines')
+            .send({year: 2015, type: 'wrong', name: 'Wine', country: 'France'})
+            .expectStatus(400)
+            .end(function (err, res, body) {
+              assert.equal(body.error, 'VALIDATION_ERROR');
+              assert.equal(body.validation.type, 'INVALID');
+              done();
+            });
+        }));
+
+      }));
+    });
+
+    describe('PUT update wines', function () {
+
+      describe('while validating', sinon.test(function () {
+
+        this.stub(Wine.prototype, 'save', function mockSave (cb) { return this.validate(cb); });
+
+        it('will fail for invalid type value', sinon.test(function (done) {
+            this.stub(Wine, 'findOne', function mockFindOne (opt, cb) {
+                cb(null, wineList[1]);
+            });
+          api().put('/wines/15')
+            .send({type: 'wrong'})
+            .expectStatus(400)
+            .end(function (err, res, body) {
+              assert.notEqual(body, undefined);
+              assert.equal(body.error, 'VALIDATION_ERROR');
+              assert.equal(body.validation.type, 'INVALID');
+              done();
+            });
+        }));
+
+      }));
+    });
+
   });
 });
