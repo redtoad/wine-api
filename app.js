21335a5ac2138ccea152ced7234e881b1c3a7966
Sebastian Rahlf <basti@redtoad.de>
2017-04-14 14:58:12 +0200

    Fix complaints for source.

--- app.js
+++ app.js
@@ -2,19 +2,16 @@
 'use strict';
 
 var mongoose = require('mongoose');
-var http = require('http');
-
-var models = require('./lib/models');
 var server = require('./lib/server');
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/test';
 const PORT = process.env.PORT || 8080;
 
 mongoose.connection.on('connected', function () {
-    console.log('Mongoose default connection open to ' + MONGODB_URI);
+  console.log('Mongoose default connection open to ' + MONGODB_URI);
 });
 
-mongoose.connection.on('error',function (err) {
+mongoose.connection.on('error', function (err) {
   console.log('Mongoose default connection error: ' + err);
 });
 
@@ -22,7 +19,7 @@ mongoose.connection.on('disconnected', function () {
   console.log('Mongoose default connection disconnected');
 });
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
   mongoose.connection.close(function () {
     console.log('Mongoose default connection disconnected through app termination');
     process.exit(0);
